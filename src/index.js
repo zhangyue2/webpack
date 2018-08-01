63aeed3937b382c261bec03cc519256a73237134
zhangyue <zhangyue@quarkioe.com>
2018-08-01 15:59:46 +0800

    模块热替换

--- src/index.js
+++ src/index.js
@@ -13,8 +13,17 @@ function component() {
 
   element.appendChild(btn);
 
-
   return element;
 };
 
-document.body.appendChild(component());
+let element = component();
+document.body.appendChild(element);
+
+if (module.hot){
+  module.hot.accept('./print.js', function() {
+    console.log('Accepting the update printMe module!');
+    document.body.removeChild(element);
+    element = component();
+    document.body.appendChild(element);
+  })
+};
